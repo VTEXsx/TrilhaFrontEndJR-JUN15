c792ff2bf7034caa4a3763fd14d741848eb97746
VTEXsx <8221vitor@gmail.com>
2024-07-08 17:37:20 -0300

    configuração final

--- trilhafrontend/vite.config.js
+++ trilhafrontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        /^node:.*/, // Esta expressão regular corresponde a qualquer string que comece com "node:"
+      ],
+    },
+  },
+});
